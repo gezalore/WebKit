6dc64013940336e5d30aa1401e143b27cba3a836
Geza Lore <gezalore@gmail.com>
2022-04-12 09:33:13 +0100

    Various minor tweaks for BBQ on ARMv7
    - Do not attempt tier up to OMG
    - Do not use 64-bit ops for initializing 32-bit locals
    - Tweak AirOpcode.opcodes/opcode_generator.rb to support ARMv7/Thumb-2

--- Source/JavaScriptCore/wasm/WasmBBQPlan.cpp
+++ Source/JavaScriptCore/wasm/WasmBBQPlan.cpp
@@ -106,7 +106,10 @@ void BBQPlan::work(CompilationEffort effort)
 
     CompilationContext context;
     Vector<UnlinkedWasmToWasmCall> unlinkedWasmToWasmCalls;
-    std::unique_ptr<TierUpCount> tierUp = makeUnique<TierUpCount>();
+    std::unique_ptr<TierUpCount> tierUp;
+#if !CPU(ARM) // We don't want to attempt tier up to OMG on ARM just yet. Not initialising this counter achieves just that.
+    tierUp = makeUnique<TierUpCount>();
+#endif
     std::unique_ptr<InternalFunction> function = compileFunction(m_functionIndex, context, unlinkedWasmToWasmCalls, tierUp.get());
 
     LinkBuffer linkBuffer(*context.wasmEntrypointJIT, nullptr, LinkBuffer::Profile::Wasm, JITCompilationCanFail);
